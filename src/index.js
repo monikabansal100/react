d445e2bb4c554c5cfcc12c470d1407627e0bcbd2
laptop-mb <bansalmonika143@gmail.com>
2023-01-18 21:48:21 +0530

    google

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const LogoComponent=(props)=>{
+  return <img alt='' className='image' src='https://pngimg.com/uploads/google/google_PNG19625.png'></img>
+}
+
+const WordBoxComponent=(props)=>{
+  return <input className='input' placeholder='Search Google or Type a URL'></input>
+}
+
+const SearchButton=()=>{
+  return <div className='button'>
+    <Button1/><Button2/>
+    </div>
+}
+const Button1 =()=>{
+  return <button className='btn1' type='button'>Google Search</button>
+}
+const Button2=()=>{
+  return <button className='btn2' type="button">I'am Feeling Lucky</button>
+}
+
+
+const LanguageComponent=()=>{
+  return <p className='lang'>Google in:English</p>
+}
+
+
+setInterval(()=>{
+  root.render(
+    <React.StrictMode>
+      <LogoComponent/>
+      <WordBoxComponent />
+      <SearchButton/>
+      <LanguageComponent/>
+    </React.StrictMode>
+  );
+},1000);
+
+
